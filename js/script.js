e1a28f498f003643dcb9c4dea6e394f5c80b7044
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-10-27 18:34:29 +0000

    a

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ async function addGraph() {
     graph[source] = {};
     edges.push({ from: source, to: target, weight, label:weight.toString() });
     graph[source][target] = weight;
-    graph[target][source] = weight; 
+    //graph[target][source] = weight; 
   }
   // Criar a visualização do grafo
   new vis.Network(container, data, options);
